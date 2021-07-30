d941707d96b9fd5cdd61cd46dfe0640f18ea3820
AppreciativeGeek <mnpencildoodle@gmail.com>
2021-07-29 22:39:17 -0500

    Correctly implemented data fetching from API and also data parsing.

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -15,6 +15,31 @@ struct ContentView: View {
             List (searchResults) { result in
                 Text(result.title)
             }
+            .navigationTitle("Google Search")
+        }
+        .onAppear(perform: {
+            getSearch()
+        })
+    }
+    
+    func getSearch() {
+        let apiKey = "?rapidapi-key=0a1cfe84e9msh52abeacda017807p1a74c5jsn30e27789404c"
+        let query = "https://google-search3.p.rapidapi.com/api/v1/search/q=funny+meme&num=100\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                print(json)
+                let contents = json["results"].arrayValue
+                if contents.isEmpty == false {
+                    for item in contents {
+                        let title = item["title"].stringValue
+                        let link = item["link"].stringValue
+                        let description = item["description"].stringValue
+                        let result = Result(title: title, link: link, description: description)
+                        searchResults.append(result)
+                    }
+                }
+            }
         }
     }
 }
